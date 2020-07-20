1c280aa5c2d08784e61d991e7881c2bfda20a010
Rômulo Ponciano <romulo_37@hotmail.com>
2020-07-20 11:56:00 -0300

    removing List use. It's unnecessary

--- src/main/java/net/intelie/challenges/EventStoreImpl.java
+++ src/main/java/net/intelie/challenges/EventStoreImpl.java
@@ -1,13 +1,11 @@
 package net.intelie.challenges;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class EventStoreImpl implements EventStore {  
 
-  private final ConcurrentHashMap<String, List<AutomaticConcurrenceHashMap>> events = new ConcurrentHashMap<>();
+  private final ConcurrentHashMap<String, AutomaticConcurrenceHashMap> events = new ConcurrentHashMap<>();
 
   /**
      * Stores an event
@@ -15,17 +13,15 @@ public class EventStoreImpl implements EventStore {
      * @param event
      */
     public void insert(Event event) {
-      AutomaticConcurrenceHashMap newEvent = new AutomaticConcurrenceHashMap();
-      newEvent.autoKeyPut(event);
-      // if already have event type list, then just get and add to it
+      // if already have event type, then just get and add to it
       if(events.containsKey(event.type())) {
-        events.get(event.type()).add(newEvent);
+        events.get(event.type()).autoKeyPut(event);
       } 
-      // else, create the new list and insert new event to it
+      // else, create the new concurrenthashmap and insert new event to it
       else {
-        List<AutomaticConcurrenceHashMap> newList = new ArrayList<>();
-        newList.add(newEvent);
-        events.put(event.type(), newList);
+        AutomaticConcurrenceHashMap newEvent = new AutomaticConcurrenceHashMap();
+        newEvent.autoKeyPut(event);
+        events.put(event.type(), newEvent);
       };
     };
 
@@ -52,16 +48,14 @@ public class EventStoreImpl implements EventStore {
     public EventIterator query(String type, long startTime, long endTime) {
       if(type != null && events.containsKey(type)) {
         AutomaticConcurrenceHashMap result = new AutomaticConcurrenceHashMap();
-        // get the list with all AutomaticConcurrenceHashMaps of determinated type
-        for(AutomaticConcurrenceHashMap typeHash : events.get(type)) {
-          // for each entry in list, check if timestamp is between 
-          // startTime (inclusive) and endTime (exclusive)
-          for(Map.Entry<String, Event> entry : typeHash.entrySet()) {
-            Event auxEvent = entry.getValue();
-            long timestamp = auxEvent.timestamp();
-            if(timestamp >= startTime && timestamp < endTime) {
-              result.autoKeyPut(entry.getValue());
-            };
+        // get the concurrenthashmap with all events of determinated type
+        // for each entry in map, check if timestamp is between
+        // startTime (inclusive) and endTime (exclusive)
+        for(Map.Entry<String, Event> entry : events.get(type).entrySet()) {
+          Event auxEvent = entry.getValue();
+          long timestamp = auxEvent.timestamp();
+          if(timestamp >= startTime && timestamp < endTime) {
+            result.autoKeyPut(entry.getValue());
           };
         };
         return new EventIteratorImpl(result);
